bb3fcca4023001fc4990e507bf5013d83e1dc1d2
Jeff Levine <jlevine_84@yahoo.com>
2018-12-28 14:25:06 -0500

    updates to styling, JS objects, and layout

---
+++ assets/javascript/game.js
@@ -0,0 +1,23 @@
+var YTS {
+    name: "Yellow Toothed Spider",
+    health: 100,
+    attack: 12
+};
+
+var WT {
+    name: "Wattely Turkey",
+    health: 125,
+    attack: 10
+};
+
+var BKCC {
+    name: "Bottle of Korean Cosmetic Chemicals",
+    health: 150,
+    attack: 8
+};
+
+var SCC {
+    name: "Stuffy Cheeked Chipmunk",
+    health: 175,
+    attack: 6
+};
