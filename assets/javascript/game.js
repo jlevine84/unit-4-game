3db63b7b2f444bf3eb451bb4913bf5e38eb52146
Jeff Levine <jlevine_84@yahoo.com>
2019-01-01 16:59:26 -0500

    code updates and images

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,23 +1,35 @@
-var YTS {
+var YTS = {
     name: "Yellow Toothed Spider",
     health: 100,
     attack: 12
 };
 
-var WT {
-    name: "Wattely Turkey",
+var WT = {
+    name: "Wattley Turkey",
     health: 125,
     attack: 10
 };
 
-var BKCC {
-    name: "Bottle of Korean Cosmetic Chemicals",
+var BT = {
+    name: "Beaver Teeth",
     health: 150,
     attack: 8
 };
 
-var SCC {
+var SCC = {
     name: "Stuffy Cheeked Chipmunk",
     health: 175,
     attack: 6
 };
+
+$(document).ready(function() {
+
+    $("#YTS").on("click", function () {
+        
+        charset = $("<img>").attr("src", "assets/images/Spider.jpg").addClass("char-select");
+        $("#character").append(charset);
+        
+        
+    });
+
+});
